badf833ddf4c884af33753beef51d572df792eef
Michael Krausse <michael.krausse@directbox.com>
2020-05-01 06:31:00 +0200

    renders consumer

--- poc_dependency_gui/src/Overview.js
+++ poc_dependency_gui/src/Overview.js
@@ -7,25 +7,47 @@ class Overview extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {keys: ["", "gradleVersion", "org.junit.jupiter:junit-jupiter", "com.nhaarman.mockito", "kotlin2:mockito-kotlin"]};
+        this.state = {
+            keys: ["gradleVersion", "org.junit.jupiter:junit-jupiter", "com.nhaarman.mockitokotlin2:mockito-kotlin"],
+            "content": [
+                {
+                    "projectName": "imglib",
+                    "version": "1.4.0",
+                    "directDependencies": {
+                        "gradleVersion": "6.3",
+                        "org.junit.jupiter:junit-jupiter": "5.6.2",
+                        "com.nhaarman.mockitokotlin2:mockito-kotlin": "2.2.0"
+                    }
+                }
+            ]
+        };
+
+    }
+
+    renderConsumer(element, displayKeys) {
+      return displayKeys.map((keyValue) => <td>{element.directDependencies[keyValue]}</td>);
     }
 
     render() {
         const headline = this.state.keys.map((element) => <th>{element}</th>);
-
+        const content = this.state.content.map((element) => <tr><td>{element.projectName}</td>{this.renderConsumer(element, this.state.keys)}</tr>)
         return (
             <div>
                 <table>
                     <tr>
+                        <th>version</th>
                         {headline}
                     </tr>
-
+                    {content}
                 </table>
-                <h1>hallo</h1>
+                <div>
+                    generated from poc-dependencies, version: 0.0.1, Copyright Michael Krauße 2020
+                </div>
             </div>
+
         );
     }
 
-};
+}
 
 export default Overview;
