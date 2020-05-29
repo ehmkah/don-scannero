0c473d81e913ebacf419f544ded550ae9db38417
Michael Krausse <michael.krausse@directbox.com>
2020-05-29 21:46:06 +0200

    write dependency depends_on correctly to neo4j

--- poc_dependency_gradle/neo4j_writer.py
+++ poc_dependency_gradle/neo4j_writer.py
@@ -4,13 +4,11 @@ from extractor import parseDependency
 from src.repositories.artifact_repository import ArtifactRepository
 
 result = subprocess.run(["./gradlew", "dependencies"], capture_output=True)
-dependencies = parseDependency(str(result.stdout))
+artifact = parseDependency(str(result.stdout))
 
 
 def run():
     artifactory_repository = ArtifactRepository("bolt://localhost:7687", "", "")
-    for dependency in dependencies.depends_on:
-        artifactory_repository.write(dependency)
-
+    artifactory_repository.write(artifact)
 
 run()
--- poc_dependency_gradle/src/repositories/artifact_repository.py
+++ poc_dependency_gradle/src/repositories/artifact_repository.py
@@ -14,4 +14,18 @@ class ArtifactRepository:
 
     def write(self, artifact: Artifact):
         with self._driver.session() as session:
-            session.run("CREATE (a:Artifact {artifact_name: $artifact_name})", artifact_name=artifact.artifact_name)
+            self.write_artifact(artifact, session)
+            for dependency in artifact.depends_on:
+                self.write_artifact(dependency, session)
+                session.run("MATCH(a: Artifact), (b:Artifact)  "
+                            "WHERE a.artifact_name = $source_artifact_name AND a.artifact_version= $source_artifact_version "
+                            "AND b.artifact_name = $dependency_artifact_name AND b.artifact_version = $dependency_artifact_version"
+                            " CREATE(a) - [r: DEPENDS_ON]->(b)",
+                            source_artifact_name=artifact.artifact_name,
+                            source_artifact_version=artifact.artifact_version,
+                            dependency_artifact_name=dependency.artifact_name,
+                            dependency_artifact_version=dependency.artifact_version)
+
+    def write_artifact(self, artifact, session):
+        session.run("CREATE (a:Artifact {artifact_name: $artifact_name, artifact_version: $artifact_version})",
+                    artifact_name=artifact.artifact_name, artifact_version=artifact.artifact_version)
