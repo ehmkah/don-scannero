944c8378b27f94bd167f0753ef5ebaf8e97ec08a
Michael Krausse <michael.krausse@directbox.com>
2020-11-14 11:23:38 +0100

    removed python-way

--- poc_dependency_gradle/console_printer.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-from extractor import parseDependency
-
-result = subprocess.run(["./gradlew", "dependencies"], capture_output=True)
-dependencies = parseDependency(str(result.stdout))
-
-for dependency in dependencies.depends_on:
-    print(dependency.artifact_name+":"+dependency.artifact_version)
--- poc_dependency_gradle/model.py
+++
@@ -1,8 +0,0 @@
-class Artifact:
-    artifact_name = ""
-    artifact_version = ""
-    depends_on = []
-
-    def __init__(self, artifact_name, artifact_version):
-        self.artifact_name = artifact_name
-        self.artifact_version = artifact_version
