02b6d2fc29970a26acc64e50c1b833376bfbc66f
Michael Krausse <michael.krausse@directbox.com>
2020-11-27 19:58:01 +0100

    adding depedency

--- poc_dependency_integrator/src/main/java/com/github/foo/Integrator.java
+++ poc_dependency_integrator/src/main/java/com/github/foo/Integrator.java
@@ -1,12 +1,20 @@
 package com.github.foo;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
+/**
+ * Does the work - take a build.gradle and add all required information so don scannero can run.
+ */
 public class Integrator {
 
-    public OutputStream append(InputStream resourceAsStream) {
-        
-        return null;
+    public List<String> integrate(final File inputFile) throws IOException {
+        GradleDependencyUpdater updater = new GradleDependencyUpdater(inputFile);
+
+        FindDependenciesVisitor visitor = updater.insertDependency(
+                "        classpath \"com.github.ehmkah:don-scannero:0.0.7-SNAPSHOT\"");
+
+        return updater.getGradleFileContents();
     }
 }
--- poc_dependency_integrator/src/test/java/com/github/ehmkah/foo/IntegratorTest.java
+++ poc_dependency_integrator/src/test/java/com/github/ehmkah/foo/IntegratorTest.java
@@ -17,22 +17,16 @@ public class IntegratorTest {
 
     @Test
     public void testExampl1() throws IOException {
+        // GIVEN
         final File inputFile = new File("src/test/resources/given.example1.gradle");
-        final File outputFile = new File("src/test/resources/expected.example1.gradle");
-
-        GradleDependencyUpdater updater = new GradleDependencyUpdater(inputFile);
-        GradleDependencyUpdater updater2 = new GradleDependencyUpdater(outputFile);
-
-        FindDependenciesVisitor visitor = updater.insertDependency(
-                "compile \"com.github.ehmkah.don-scannero1.0.0\"");
-
-        //int dependenceLineNum = visitor.getDependenceLineNum();
-
-        //assertEquals(20, dependenceLineNum);
-
-        List<String> actualFileContent = updater.getGradleFileContents();
+        final File expectedFile = new File("src/test/resources/expected.example1.gradle");
+        GradleDependencyUpdater updater2 = new GradleDependencyUpdater(expectedFile);
         List<String> expectedFileContent = updater2.getGradleFileContents();
 
+        // WHEN
+        List<String> actualFileContent = sut.integrate(inputFile);
+
+        // THEN
         assertEquals(expectedFileContent, actualFileContent);
     }
 }
